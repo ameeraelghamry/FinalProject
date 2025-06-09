a3b6844e52748e7ee47f57d978ed30561df1e775
ameeraelghamry <ameeraelghamry@gmail.com>
2025-06-09 03:25:59 +0300

    created language switcher

--- app.js
+++ app.js
@@ -5,29 +5,37 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const session = require('express-session');
 const path = require('path');
+const i18n = require('i18n');
+const cookieParser = require('cookie-parser');
+
 require('dotenv/config')
 
-
-//routers
 const api = process.env.API_URL
 const carsRoutes = require('./routers/cars')
 const userRoutes = require('./routers/users')
-//const rentalRoutes = require('./routers/rentals')
 const adminRoutes = require('./routers/admins');
-
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(cookieParser());
 
-//middleware
-app.use(bodyParser.json())
-app.use(morgan('tiny')) //good to display log requests
+i18n.configure({
+  locales: ['en', 'fr'],
+  directory: path.join(__dirname, 'locales'),
+  defaultLocale: 'en',
+  queryParameter: 'lang',
+  cookie: 'locale',
+  autoReload: true,
+  syncFiles: true
+});
+app.use(i18n.init);
 
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 app.use(session({
   secret: 'your_secret_key',
@@ -36,29 +44,28 @@ app.use(session({
   cookie: { secure: false }
 }));
 
+// Make locale available in all views
+app.use((req, res, next) => {
+  res.locals.locale = req.getLocale();
+  res.locals.__ = res.__ = req.__; // pass __ function to views for convenience
+  next();
+});
 
-//main routes of products
-app.use(`${api}/cars`, carsRoutes)
-app.use(`${api}/users`, userRoutes)
-//app.use(`${api}/rentals`, rentalRoutes)
-app.use(`${api}/admin`, adminRoutes)
+// Then mount routes
+app.use(`${api}/cars`, carsRoutes);
+app.use(`${api}/users`, userRoutes);
+app.use(`${api}/admin`, adminRoutes);
 
-mongoose
-  .connect(process.env.CONNECTION_STRING)
-  .then(() => {
-    console.log('Database connection is ready...')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
+mongoose.connect(process.env.CONNECTION_STRING)
+  .then(() => console.log('Database connection is ready...'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.render('home', { user: req.session?.user });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('ContactUs', { user: req.session.user });
+  res.render('ContactUs', { user: req.session?.user });
 });
 
 app.get('/about', (req, res) => {
@@ -81,9 +88,14 @@ app.get('/explore', (req, res) => {
   res.render('Explore', { user: req.session?.user });
 });
 
-//listening port for server
+// Language switcher
+app.get('/lang/:locale', (req, res) => {
+  const locale = req.params.locale;
+  res.cookie('locale', locale, { maxAge: 900000, httpOnly: false });
+  const backURL = req.header('Referer') || '/';
+  res.redirect(backURL);
+});
+
 app.listen(3000, () => {
   console.log('server is running on port 3000')
-})
-
-//example for testing
+});
